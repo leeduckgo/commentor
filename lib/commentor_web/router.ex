ee649f38ab9c5d1e86a3e0345817908129acc249
leeduckgo <albertschr@163.com>
2021-01-22 15:48:56 +0800

    init

--- lib/commentor_web/router.ex
+++ lib/commentor_web/router.ex
@@ -6,7 +6,7 @@ defmodule CommentorWeb.Router do
     plug :fetch_session
     plug :fetch_live_flash
     plug :put_root_layout, {CommentorWeb.LayoutView, :root}
-    plug :protect_from_forgery
+    # plug :protect_from_forgery
     plug :put_secure_browser_headers
   end
 
